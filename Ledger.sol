441b2ab8eb52811207d03ed303348d0d3eefb558
Quincy Jones <rqjones@asu.edu>
2022-10-26 18:57:44 -0700

    implement view ledgerID and check ContractStatus

--- Ledger.sol
+++ Ledger.sol
@@ -1,27 +1,51 @@
 pragma solidity ^0.8.10;
 // SPDX-License-Identifier: MIT
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract LedgerContract{
-    string name;
-    uint totalIDs;
+contract LedgerContract is ERC1155{
+    string public name;
+    bool public status;
+    uint public contractCount = 0;
+    uint keyToken;
 
-struct Ledger{
-    address[] _contract;
-    uint _timeStamp;
-    uint _id;
-}
-
-    constructor(bool _contractStatus, string memory _name){
+    mapping(uint => Ledger) ledger;
+    struct Ledger{
+        address[] Contract;
+        uint TimeStamp;
+        uint ID;
+        bool Exist;
+    }
+    constructor(string memory _name,bool _contractStatus,uint _totalKeys,uint keyID,string memory _URI)ERC1155(_URI){
         name = _name;
-        checkContractStatus(_contractStatus);   
+        status = _contractStatus;
+        keyToken = keyID;
+        //create Ledger editor key
+        _mint(msg.sender,keyToken, _totalKeys, "");
+    }
+    //check to 
+    modifier checkPermissions(){
+            checkContractStatus();   
+        _;
     }
     //Checks to see if contract is owned by a token or its self
-    function checkContractStatus(bool _ContractStatus)internal returns(bool){}
-    //Add a new contract to the ledger
-    function addContract(address _contract)public view returns(uint){}   
+    function checkContractStatus()internal returns(bool){
+        if(status == false){
+            require(balanceOf(msg.sender,keyToken) > 0);
+        } else {
+            require(msg.sender == address(this), "only the contract can add to the ledger");
+        }
+    }
+    //Public function to Add a new contract to the ledger
+    function addContract(address[] memory _contract)public checkPermissions returns(uint){
+        return _addContract(_contract);
+    }   
+    //Internal function to add to the ldeger based on additional code
+    function _addContract(address[] memory _contract)internal returns(uint){
+        ledger[contractCount] = Ledger(_contract,block.timestamp,contractCount,true);
+        contractCount++;
+    }  
     //View leder by ID#
-    function viewLedgerID(uint _ID)public view returns(address[] memory,uint,uint){} 
-    //check if contract exist by ID#  
-    function viewLedgerExist(address _contract)public view returns(bool){}   
+    function viewLedgerID(uint _ID)public view returns(address[] memory,uint,uint,bool){
+        return (ledger[_ID].Contract,ledger[_ID].TimeStamp,ledger[_ID].ID,ledger[_ID].Exist);
+    } 
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Ledger-Contract
+# ContractLedger
 A DApp or User may hold a ledger contract to keep track of Contract Addresses generated or accounted for Smart contracts
