9401b42c0c543d92993e96570ce9736bdc68f25d
Quincy Jones <rqjones@asu.edu>
2022-10-27 21:55:42 -0700

    ledger contract Int
    integer version of Ledger_Contract

--- contracts/Ledger-Contract.sol
+++ contracts/Ledger-Contract.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.10;
 // SPDX-License-Identifier: MIT
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract LedgerContract {
     uint public contractCount = 0;
@@ -20,5 +19,26 @@ contract LedgerContract {
     //View leder by ID#
     function viewLedgerID(uint _ID)public view returns(address[] memory,uint,uint,bool){
         return (ledger[_ID].Contract,ledger[_ID].TimeStamp,ledger[_ID].ID,ledger[_ID].Exist);
+    } 
+}
+
+contract LedgerContractInt {
+    uint public contractCount = 0;
+
+    mapping(uint => Ledger) ledger;
+    struct Ledger{
+        uint[] _slotInt;
+        uint TimeStamp;
+        uint ID;
+        bool Exist;
+    }
+    //Internal function to add to the ldeger based on additional code
+    function addSlotInt(uint[] memory _contract)internal returns(uint){
+        ledger[contractCount] = Ledger(_contract,block.timestamp,contractCount,true);
+        return contractCount++;
+    }  
+    //View leder by ID#
+    function viewLedgerID(uint _ID)public view returns(uint[] memory,uint,uint,bool){
+        return (ledger[_ID]._slotInt,ledger[_ID].TimeStamp,ledger[_ID].ID,ledger[_ID].Exist);
     } 
 }
